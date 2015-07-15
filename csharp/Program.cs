a7c55424e987ef0ae70a2f33705954f1d9508f19
Kate <katerebekah@gmail.com>
2015-07-15 17:42:51 -0500

    homework

---
+++ csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            foreach (char l in name)
+            {
+                if ("aefhilmnosx".Contains(l))
+                {
+                    Console.WriteLine("Give me an ....  {0}", l);
+                }
+                else Console.WriteLine("Give me a ..... {0}", l);
+            }
+            Console.WriteLine("{0} is GRAND", name);
+            Console.ReadLine();
+        }
+        
+    }
+}
